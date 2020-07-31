f34dd4e1a2c1debc4082e895d82f347af57d0681
mss <singh7179@gmail.com>
2020-07-31 14:44:52 +0700

    Updates

---
+++ precache-manifest.642566c1aa5d475a8210cefed27b82de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5304fa3d70bd6492a94037ab647dd201",
+    "url": "/rrunderconstruction/index.html"
+  },
+  {
+    "revision": "5165d640fe0eb95de58d",
+    "url": "/rrunderconstruction/static/css/main.c51ef8d2.chunk.css"
+  },
+  {
+    "revision": "fd46bec511381bbb92a2",
+    "url": "/rrunderconstruction/static/js/2.21b4274f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rrunderconstruction/static/js/2.21b4274f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5165d640fe0eb95de58d",
+    "url": "/rrunderconstruction/static/js/main.dfd4a2f5.chunk.js"
+  },
+  {
+    "revision": "f60a8e04ac2cec566e68",
+    "url": "/rrunderconstruction/static/js/runtime-main.ba5948dd.js"
+  },
+  {
+    "revision": "ff85fb24c326592edc54acade9f85c2b",
+    "url": "/rrunderconstruction/static/media/construction.ff85fb24.gif"
+  }
+]);
