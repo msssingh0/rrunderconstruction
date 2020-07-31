a19386d8a51166bff48dc1a23aa1d77565b6877a
mss <singh7179@gmail.com>
2020-07-31 14:43:04 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='video-container'>
+			<div className='centered'>
+				<div className='firstLine'>
+					We are building something amazing. Stay tuned for updates.
+				</div>
+				<div className='secondLine'>
+					We can't wait to help you again with your dream home!
+				</div>
+				<div className='thirdLine'>
+					For enquiries please contact us at sales@rajrealtygroup.com or call
+					+91 9833758827
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
